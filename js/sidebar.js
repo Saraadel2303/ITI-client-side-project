6955eb821037ec76067ddb38f7ebfcc38c37efce
bsm2 <basmatalaat2@gmail.com>
2025-08-28 12:16:43 +0300

    update sidebar

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,23 +1,36 @@
-let role = localStorage.getItem("role") ?? 'employee';
+const loggedInUser = JSON.parse(localStorage.getItem("loggedInUser"));
+let role = loggedInUser?.role?.toLowerCase();
 
 if (role !== "security") {
-    attendance.style.display = "none"
+  attendance.style.display = "none";
+} else {
+    attendance.style.display = "";
 }
-
 
 if (role !== "employee") {
-    my_attendance.style.display = "none"
-    req.style.display = "none"
-    my_tasks.style.display = "none"
+  my_attendance.style.display = "none";
+  req.style.display = "none";
+  my_tasks.style.display = "none";
+} else {
+  my_attendance.style.display = "";
+  req.style.display = "";
+  my_tasks.style.display = "";
 }
 
-
 if (role !== "manger") {
-    team_tasks.style.display = "none"
-    team_attendance.style.display = "none"
-    approval.style.display = "none"
+  team_tasks.style.display = "none";
+  team_attendance.style.display = "none";
+  approval.style.display = "none";
+} else {
+  team_tasks.style.display = "";
+  team_attendance.style.display = "";
+  approval.style.display = "";
 }
 
 if (role !== "hr") {
-    settings.style.display = "none"
+  settings.style.display = "none";
+  reports.style.display = "none";
+} else {
+  settings.style.display = "";
+  reports.style.display = "";
 }
