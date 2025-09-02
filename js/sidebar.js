69c9ae8d916a2fff7d1993c08bc2c5cc558213d0
bsm2 <basmatalaat2@gmail.com>
2025-09-02 19:15:21 +0300

    fix sidebar

--- js/sidebar.js
+++ js/sidebar.js
@@ -2,13 +2,23 @@ const loggedInUser = JSON.parse(localStorage.getItem("loggedInUser"));
 let role = loggedInUser?.role?.toLowerCase();
 
 $(function () {
+  $("body").hasClass("dark-theme");
+  const theme = localStorage.getItem("theme");
+  if (theme == "dark") {
+    $("body").addClass("dark-theme");
+    $("#toggleTheme").html('<i class="bi bi-sun me-2"></i>Light Mode');
+  } else {
+    $("body").removeClass("dark-theme");
+    $("#toggleTheme").html('<i class="bi bi-moon-stars me-2"></i>Dark Mode');
+  }
   $("#toggleTheme").on("click", function (e) {
     e.preventDefault();
     $("body").toggleClass("dark-theme");
-
     if ($("body").hasClass("dark-theme")) {
+      localStorage.setItem("theme", "dark");
       $(this).html('<i class="bi bi-sun me-2"></i>Light Mode');
     } else {
+      localStorage.setItem("theme", "light");
       $(this).html('<i class="bi bi-moon-stars me-2"></i>Dark Mode');
     }
   });
@@ -44,7 +54,7 @@ if (role !== "hr") {
   settings.style.display = "";
 }
 
-const currentPage = window.location.pathname.split("/").pop(); 
+const currentPage = window.location.pathname.split("/").pop();
 
 // Loop through all sidebar links
 document.querySelectorAll("#sidebar .nav-link").forEach((link) => {
